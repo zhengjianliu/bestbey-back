ebaaf720694ac6127416d9fa7f488ac96f80da87
jdhofmann <justindhofmann@gmail.com>
2020-11-08 13:40:17 -0500

    added tv creation

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,4 +1,4 @@
 class ProductSerializer < ActiveModel::Serializer
   # include FastJsonapi::ObjectSerializer
-  attributes :id, :name, :rating, :frontimg, :sideimg, :backimg, :additional_specs, :brand, :product_options, :product_option_values, :skus
+  attributes :id, :name,:category, :rating, :brand, :frontimg, :sideimg, :backimg, :additional_specs,  :product_options,  :product_option_values, :skus
 end
